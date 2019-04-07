d5f606a8f572fa89322c373c746a696892a793bf
jaepun <jackyphung.393@gmail.com>
2019-04-07 18:04:37 -0400

    added axios with endpoints

--- client/src/components/Pages/Home/Home.js
+++ client/src/components/Pages/Home/Home.js
@@ -6,10 +6,19 @@ import React, { Component } from 'react';
 import { NavFooter, ContentArea, ContentBlock } from 'Layout';
 import { FooterContent } from 'Sections';
 import './Home.css';
+import axios from 'axios'
 
 import { UserAuthSubscriber } from 'services';
 
+const client_id = 'yy3fdkdw0g8jvhzqxnujoub0w45otv';
+
+const helix = axios.create({
+	baseURL: 'https://api.twitch.tv/helix/',
+	headers: {'Client-ID': client_id}
+});
+
 class Home extends Component {
+
 	constructor(props) {
 		super(props);
 	}
@@ -41,6 +50,18 @@ class Home extends Component {
     } else {
       document.title = `${document.title} | ${page}`;
     }
+	}
+
+	getUserData() {
+		this.helix.get('users').then(function (response) {
+			this.setState({user_data: response.data})
+		});
+	}
+
+	getStreamData() {
+		this.helix.get('streams').then(function (response) {
+			this.setState({user_data: response.data})
+		});
 	}
 
 	render() {
