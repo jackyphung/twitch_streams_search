83246de1233d0dc1a587e9ab205bb494b17c8324
jaepun <jackyphung.393@gmail.com>
2019-04-07 21:08:57 -0400

    hooking it up

--- client/src/components/Pages/Home/Home.js
+++ client/src/components/Pages/Home/Home.js
@@ -2,47 +2,43 @@
 Component
 Home
 */
-import React, { Component } from 'react';
-import { NavFooter, ContentArea, ContentBlock } from 'Layout';
-import { FooterContent } from 'Sections';
+import React, { Component } from "react";
+import { NavFooter, ContentArea, ContentBlock } from "Layout";
+import { FooterContent } from "Sections";
 //import TwitchLogo from 'assets/svg/Twitch_Purple_RGB.svg';
-import axios from 'axios';
-import './Home.css';
+import axios from "axios";
+import "./Home.css";
 
-import { UserAuthSubscriber } from 'services';
+import { UserAuthSubscriber } from "services";
 
-const client_id = 'yy3fdkdw0g8jvhzqxnujoub0w45otv';
+const client_id = "yy3fdkdw0g8jvhzqxnujoub0w45otv";
 
 const helix = axios.create({
-	baseURL: 'https://api.twitch.tv/helix/',
-	headers: {'Client-ID': client_id}
+  baseURL: "https://api.twitch.tv/helix/",
+  headers: { "Client-ID": client_id }
 });
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-	constructor(props) {
-		super(props);
-	}
+  state = {};
 
-	state = {  
-		
-	}
-
-	static getDerivedStateFromProps(nextProps, prevState) {
-    let state = { };
+  static getDerivedStateFromProps(nextProps, prevState) {
+    let state = {};
     if (prevState) {
       for (let key in nextProps) {
         if (prevState.hasOwnProperty(key)) {
-          if (nextProps[key] !== prevState[key])
-            state[key] = nextProps[key];
+          if (nextProps[key] !== prevState[key]) state[key] = nextProps[key];
         }
       }
     }
-    
+
     return state;
   }
 
-	componentDidMount() {
+  componentDidMount() {
     let page = "Search A Streamer!";
     if (document.title.includes(" | ")) {
       let title = document.title.split(" | ");
@@ -50,58 +46,71 @@ class Home extends Component {
       document.title = title.join(" | ");
     } else {
       document.title = `${document.title} | ${page}`;
-		}
-	}
+    }
+  }
 
-	getUserData(e) {
-		helix.get('users', {
-			params: {
-				login: e.target.value
-			}
-		}).then(function (response) {
-			this.setState({user_data: response.data["data"][0]})
-		});
-	}
+  getUserData(e) {
+    if (e.target.value.trim().length) {
+      helix
+        .get("users", {
+          params: {
+            login: e.target.value
+          }
+        })
+        .then(function(response) {
+          this.setState({ user_data: response.data["data"][0] });
+        });
+    }
+  }
 
-	getStreamData() {
-		helix.get('streams', {
-			params: {
-				user_id : this.state.user_data
-			}
-		}).then(function (response) {
-			this.setState({stream_data: response.data["data"][0]})
-		});
-	}
+  getStreamData() {
+    helix
+      .get("streams", {
+        params: {
+          user_id: this.state.user_data
+        }
+      })
+      .then(function(response) {
+        this.setState({ stream_data: response.data["data"][0] });
+      });
+  }
 
-	render() {
-		return (
-			<UserAuthSubscriber>
-				{ auth => (
-					<ContentArea FooterContent={<FooterContent/>}>
-						<ContentBlock className="v-padding-25" style={{ minHeight: "calc(100vh - 96px)", textAlign: "center" }} size="10">
-							<input type="text" placeholder="Search Twitch Streamer" />
-							{ Object.keys(this.state).length ?
-								<React.Fragment>
-									
-								</React.Fragment>
-								:
-								<React.Fragment>
-									<h1 style={{ fontWeight: "normal" }}>
-										Welcome visitor! You may wonder what to do with this application...
-									</h1>
-									<h2 style={{ fontWeight: "normal" }}>
-										This application serves a purpose to search for any Twitch Streamer and see their details
-									</h2>
-									{/* <TwitchLogo /> */}
-								</React.Fragment>
-							}
-						</ContentBlock>
-					</ContentArea>
-				)}
-			</UserAuthSubscriber>
-		);
-	}
-
+  render() {
+    return (
+      <UserAuthSubscriber>
+        {auth => (
+          <ContentArea FooterContent={<FooterContent />}>
+            <ContentBlock
+              className="v-padding-25"
+              style={{ minHeight: "calc(100vh - 96px)", textAlign: "center" }}
+              size="10"
+            >
+              <input
+                type="text"
+                placeholder="Search Twitch Streamer"
+                onChange={this.getUserData}
+              />
+              {Object.keys(this.state).length ? (
+                <React.Fragment />
+              ) : (
+                <React.Fragment>
+                  <h1 style={{ fontWeight: "normal" }}>
+                    Welcome visitor! You may wonder what to do with this
+                    application...
+                  </h1>
+                  <h2 style={{ fontWeight: "normal" }}>
+                    This application serves a purpose to search for any Twitch
+                    Streamer and see their details
+                  </h2>
+                  {/* <TwitchLogo /> */}
+                </React.Fragment>
+              )}
+            </ContentBlock>
+          </ContentArea>
+        )}
+      </UserAuthSubscriber>
+    );
+  }
 }
 
 export { Home };
